a99c6fdc0f88189426ca22df2bd572f53877d66b
Andrey Malets <malets@yandex-team.ru>
2026-03-02 21:34:32 +0500

    Simplify Makefile, scripts and configs for Linux, more straightforward Makefile.

--- firmware/volta_box_v4/volta_box_stm32/openocd.sh
+++
@@ -1,8 +0,0 @@
-set -x
-
-TOOLS=$1; RESOURCES=$2; shift 2
-DYLD_LIBRARY_PATH="$TOOLS/openocd/lib" \
-    exec "$TOOLS/openocd/bin/openocd" \
-        --search "$RESOURCES/openocd/scripts" \
-        --search "$RESOURCES/openocd/st_scripts" \
-        "$@"
